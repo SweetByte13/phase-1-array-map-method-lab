2b890038a5eb611534957cb302d40d8e86b2e569
Tova Yocheved Hillman <hillman.t.y@gmail.com>
2024-04-09 08:45:52 -0400

    done

--- index.js
+++ index.js
@@ -12,5 +12,34 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+
+  const newList = tutorials.map(sentence => {
+    // 'what does the this keyword mean?'
+    //split string into array
+    //save new array as a variable
+    //loop through new array
+    //select the first letter of each string in the array
+    //uppercase
+    //dont change any other letters
+    //combine the strings in the array into one string
+
+    const innerList = sentence.split(' ')
+    const upperCaseWords = innerList.map(letter => {
+      return letter.charAt(0).toUpperCase() + letter.slice(1)
+    })
+
+    const upperCaseSentence = upperCaseWords.join(' ');
+
+    console.log(upperCaseSentence)
+
+    return upperCaseSentence
+
+  }
+  );
+
+  return newList
+};
+
+
+
+console.log(titleCased());
